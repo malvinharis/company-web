01f5746ecaae129dce041edfba57146c3d35750e
Muchammad Alvin Haris <alvin.haris@usetada.com>
2025-10-19 20:39:02 +0700

    chore: optimize async handler

--- src/lib/middlewares/async.ts
+++ src/lib/middlewares/async.ts
@@ -1,11 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextHandler } from 'next-connect';
 
 /**
  * Async handler middleware to wrap async route handlers
  * and catch any errors, passing them to the error handler
  */
-const asyncHandler = (fn: Function) => {
-  return (req: NextApiRequest, res: NextApiResponse, next: Function) => {
+type AsyncHandler = (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  next: NextHandler
+) => Promise<void> | void;
+
+const asyncHandler = (fn: AsyncHandler) => {
+  return (req: NextApiRequest, res: NextApiResponse, next: NextHandler) => {
     Promise.resolve(fn(req, res, next)).catch(next);
   };
 };
